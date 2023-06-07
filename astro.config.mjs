02054e0dfdeb19e5619770c2318fc1f5bbd8f5a6
Malcolm Gibbons <malcolm@texel.com.ar>
2023-06-06 22:03:18 -0300

    Iniciando las pruebas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://fournaturalenergy.com.ar/',
+  integrations: [tailwind(), sitemap(), robotsTxt()],
+  outDir: './dist',
+});
