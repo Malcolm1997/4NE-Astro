046a97e90676eee19069dc0cf99ab8daf3f35697
Malcolm Gibbons <malcolm@texel.com.ar>
2024-04-25 12:43:56 -0300

    u

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://www.fournaturalenergy.com.ar',
+  integrations: [sitemap(), react()]
+});
---
+++ src/components/react/ImageIndex.jsx
@@ -0,0 +1,13 @@
+import { animate } from "framer-motion"
+const ImageIndex = () => {
+	return (
+
+    
+            <div>
+
+            ¡Hola, mundo!
+            </div>
+	);
+};
+
+export default ImageIndex;
