3c98dfdea2e80a167652c06d53653fb9ba1009a3
Malcolm Gibbons <malcolm@texel.com.ar>
2024-06-07 17:08:40 -0300

    u

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.fournaturalenergy.com.ar',
+  site: 'https://malcolm1997.github.io/4NE-Astro/',
   integrations: [sitemap()]
 });
