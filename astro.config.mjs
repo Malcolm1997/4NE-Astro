89f69a87f84b5ff71324eda534bf7bc7b8d60ace
Malcolm Gibbons <malcolm@texel.com.ar>
2024-06-14 15:42:41 -0300

    u

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://malcolm1997.github.io',
-  base: '4NE-Astro',
   integrations: [sitemap()]
 });
